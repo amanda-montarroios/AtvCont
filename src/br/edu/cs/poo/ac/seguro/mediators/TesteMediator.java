6bdfebdf11a03e517cc6a3166b6b1b1ccd959873
Amanda Montarroios <amontarroiosoliveira@gmail.com>
2025-05-01 19:18:44 -0300

    ultimas mudancas de mediators parte 1

--- src/br/edu/cs/poo/ac/seguro/mediators/TesteMediator.java
+++
@@ -1,8 +0,0 @@
-package br.edu.cs.poo.ac.seguro.mediators;
-
-public class TesteMediator {
-    public static void main(String[] args) {
-        SeguradoMediator mediator = SeguradoMediator.getInstancia();
-        System.out.println("Mediator criado: " + mediator);
-    }
-}
