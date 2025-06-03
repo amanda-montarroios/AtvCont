56fe5248aae8aea3a682be855fffc137ffaf0b61
Malu Dantas <mldt@cesar.school>
2025-06-02 21:14:16 -0300

    1 e 2 melhorias

--- src/br/edu/cs/poo/ac/seguro/entidades/Apolice.java
+++ src/br/edu/cs/poo/ac/seguro/entidades/Apolice.java
@@ -1,19 +1,35 @@
 package br.edu.cs.poo.ac.seguro.entidades;
 
-import lombok.*;
 import java.io.Serializable;
 import java.math.BigDecimal;
 import java.time.LocalDate;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.NoArgsConstructor;
 
 @Getter
 @Setter
-@AllArgsConstructor
-public class Apolice implements Serializable {
-
+@NoArgsConstructor
+public class Apolice implements Serializable, Registro {
+    private static final long serialVersionUID = 1L;
     private String numero;
     private Veiculo veiculo;
     private BigDecimal valorFranquia;
     private BigDecimal valorPremio;
     private BigDecimal valorMaximoSegurado;
     private LocalDate dataInicioVigencia;
+
+    public Apolice(String numero, Veiculo veiculo, BigDecimal valorFranquia, BigDecimal valorPremio, BigDecimal valorMaximoSegurado, LocalDate dataInicioVigencia) {
+        this.numero = numero;
+        this.veiculo = veiculo;
+        this.valorFranquia = valorFranquia;
+        this.valorPremio = valorPremio;
+        this.valorMaximoSegurado = valorMaximoSegurado;
+        this.dataInicioVigencia = dataInicioVigencia;
+    }
+
+    @Override
+    public String getIdUnico() {
+        return this.numero;
+    }
 }
--- src/br/edu/cs/poo/ac/seguro/entidades/Veiculo.java
+++ src/br/edu/cs/poo/ac/seguro/entidades/Veiculo.java
@@ -1,19 +1,29 @@
 package br.edu.cs.poo.ac.seguro.entidades;
 
+import java.io.Serializable;
 import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
-import java.io.Serializable;
+import lombok.NoArgsConstructor;
 
 @Getter
 @Setter
 @AllArgsConstructor
-@EqualsAndHashCode
-public class Veiculo implements Serializable {
+@NoArgsConstructor
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+public class Veiculo implements Serializable, Registro {
+    private static final long serialVersionUID = 1L;
 
+    @EqualsAndHashCode.Include
     private String placa;
+
     private int ano;
     private Segurado proprietario;
     private CategoriaVeiculo categoria;
+
+    @Override
+    public String getIdUnico() {
+        return this.placa;
+    }
 }
